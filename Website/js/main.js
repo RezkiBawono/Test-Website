68cfef2360a28057ee16ce1ce89885b6cd4f16de
RezkiBawono <116043075+RezkiBawono@users.noreply.github.com>
2022-11-22 13:55:52 +0700

    Add files via upload

---
+++ Website/js/main.js
@@ -0,0 +1,14 @@
+
+
+
+// let navLinks = document.querySelector('#navLinks')
+
+// document.getElementById('showMenu').addEventListener('click', showMenu)
+// function showMenu(){
+//     navLinks.st
+// }
+
+// document.getElementById('hideMenu').addEventListener('click', hideMenu)
+// function hideMenu(){
+//     navLinks.style.right = '-200px'
+// }
